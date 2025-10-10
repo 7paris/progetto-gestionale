65a7796cb6a64d4fbd31185be40b962535222865
s03089 <s9434960s@fermibassano.edu.it>
2025-10-10 23:43:46 +0200

    Aggiunta porta

--- app.py
+++ app.py
@@ -182,5 +182,7 @@ def create_user():
     return redirect(url_for('manage_users'))
 
 
-if __name__=='__main__':
-    app.run(debug=True)
+if __name__ == '__main__':
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
